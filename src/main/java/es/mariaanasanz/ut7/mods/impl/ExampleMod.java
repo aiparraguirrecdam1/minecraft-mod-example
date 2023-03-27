52974202e807a1b5594e9a6a5b0ec6e22f9f11d7
aiparraguirrecdam1 <aiparraguirrecdam1@gmail.com>
2023-03-27 20:11:57 +0200

    Conseguido el tipo de bloque destruido (si es tierra, arena o grava imprime las posiciones, sino no)

--- src/main/java/es/mariaanasanz/ut7/mods/impl/ExampleMod.java
+++ src/main/java/es/mariaanasanz/ut7/mods/impl/ExampleMod.java
@@ -2,9 +2,42 @@ package es.mariaanasanz.ut7.mods.impl;
 
 import es.mariaanasanz.ut7.mods.base.*;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeManager;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.border.WorldBorder;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ChunkSource;
+import net.minecraft.world.level.chunk.ChunkStatus;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.entity.EntityTypeTest;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.lighting.LevelLightEngine;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.storage.LevelData;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.ticks.LevelTickAccess;
 import net.minecraftforge.client.event.MovementInputUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -16,11 +49,15 @@ import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 @Mod(DamMod.MOD_ID)
 public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStartEvent,
         IItemPickupEvent, ILivingDamageEvent, IUseItemEvent, IFishedEvent,
-        IInteractEvent, IMovementEvent {
+        IInteractEvent, IMovementEvent, LevelAccessor {
 
     public ExampleMod(){
         super();
@@ -34,14 +71,25 @@ public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStart
     @Override
     @SubscribeEvent
     public void onBlockBreak(BlockEvent.BreakEvent event) {
+
         BlockPos pos = event.getPos();
         BlockPos posX = BlockPos.of(event.getPos().getX());
-        System.out.println("Bloque destruido en la posicion "+ (pos.getX() + 1) + "X: " + pos);
-        System.out.println("Bloque destruido en la posicion "+ (pos.getY() + 1) + "X: " + pos);
-        System.out.println("Bloque destruido en la posicion "+ (pos.getZ() + 1) + "X: " + pos);
-        System.out.println("Bloque destruido en la posicion "+ (pos.getX() - 1) + "X: " + pos);
-        System.out.println("Bloque destruido en la posicion "+ (pos.getY() - 1) + "X: " + pos);
-        System.out.println("Bloque destruido en la posicion "+ (pos.getZ() - 1) + "X: " + pos);
+        BlockState tipo = event.getState();
+        Block block = event.getState().getBlock();
+
+        if (block == Blocks.GRASS_BLOCK || block == Blocks.SAND || block == Blocks.GRAVEL) {
+            System.out.println(tipo);
+            System.out.println("Bloque destruido en la posicion "+ (pos.getX() + 1) + "X: " + pos);
+            System.out.println("Bloque destruido en la posicion "+ (pos.getY() + 1) + "Y: " + pos);
+            System.out.println("Bloque destruido en la posicion "+ (pos.getZ() + 1) + "Z: " + pos);
+            System.out.println("Bloque destruido en la posicion "+ (pos.getX() - 1) + "X: " + pos);
+            System.out.println("Bloque destruido en la posicion "+ (pos.getY() - 1) + "Y: " + pos);
+            System.out.println("Bloque destruido en la posicion "+ (pos.getZ() - 1) + "Z: " + pos);
+
+        }
+        else {
+            System.out.println("El bloque destruido no es tierra");
+        }
     }
 
     @Override
@@ -116,5 +164,183 @@ public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStart
                 System.out.println("left"+event.getInput().left);
             }
         }
+    }
+
+    @Override
+    public long nextSubTickCount() {
+        return 0;
+    }
+
+    @Override
+    public LevelTickAccess<Block> getBlockTicks() {
+        return null;
+    }
+
+    @Override
+    public LevelTickAccess<Fluid> getFluidTicks() {
+        return null;
+    }
+
+    @Override
+    public LevelData getLevelData() {
+        return null;
+    }
+
+    @Override
+    public DifficultyInstance getCurrentDifficultyAt(BlockPos p_46800_) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public MinecraftServer getServer() {
+        return null;
+    }
+
+    @Override
+    public ChunkSource getChunkSource() {
+        return null;
+    }
+
+    @Override
+    public RandomSource getRandom() {
+        return null;
+    }
+
+    @Override
+    public void playSound(@Nullable Player p_46775_, BlockPos p_46776_, SoundEvent p_46777_, SoundSource p_46778_, float p_46779_, float p_46780_) {
+
+    }
+
+    @Override
+    public void addParticle(ParticleOptions p_46783_, double p_46784_, double p_46785_, double p_46786_, double p_46787_, double p_46788_, double p_46789_) {
+
+    }
+
+    @Override
+    public void levelEvent(@Nullable Player p_46771_, int p_46772_, BlockPos p_46773_, int p_46774_) {
+
+    }
+
+    @Override
+    public void gameEvent(GameEvent p_220404_, Vec3 p_220405_, GameEvent.Context p_220406_) {
+
+    }
+
+    @Override
+    public RegistryAccess registryAccess() {
+        return null;
+    }
+
+    @Override
+    public float getShade(Direction p_45522_, boolean p_45523_) {
+        return 0;
+    }
+
+    @Override
+    public LevelLightEngine getLightEngine() {
+        return null;
+    }
+
+    @Override
+    public WorldBorder getWorldBorder() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity getBlockEntity(BlockPos p_45570_) {
+        return null;
+    }
+
+    @Override
+    public BlockState getBlockState(BlockPos p_45571_) {
+        return null;
+    }
+
+    @Override
+    public FluidState getFluidState(BlockPos p_45569_) {
+        return null;
+    }
+
+    @Override
+    public List<Entity> getEntities(@Nullable Entity p_45936_, AABB p_45937_, Predicate<? super Entity> p_45938_) {
+        return null;
+    }
+
+    @Override
+    public <T extends Entity> List<T> getEntities(EntityTypeTest<Entity, T> p_151464_, AABB p_151465_, Predicate<? super T> p_151466_) {
+        return null;
+    }
+
+    @Override
+    public List<? extends Player> players() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public ChunkAccess getChunk(int p_46823_, int p_46824_, ChunkStatus p_46825_, boolean p_46826_) {
+        return null;
+    }
+
+    @Override
+    public int getHeight(Heightmap.Types p_46827_, int p_46828_, int p_46829_) {
+        return 0;
+    }
+
+    @Override
+    public int getSkyDarken() {
+        return 0;
+    }
+
+    @Override
+    public BiomeManager getBiomeManager() {
+        return null;
+    }
+
+    @Override
+    public Holder<Biome> getUncachedNoiseBiome(int p_204159_, int p_204160_, int p_204161_) {
+        return null;
+    }
+
+    @Override
+    public boolean isClientSide() {
+        return false;
+    }
+
+    @Override
+    public int getSeaLevel() {
+        return 0;
+    }
+
+    @Override
+    public DimensionType dimensionType() {
+        return null;
+    }
+
+    @Override
+    public boolean isStateAtPosition(BlockPos p_46938_, Predicate<BlockState> p_46939_) {
+        return false;
+    }
+
+    @Override
+    public boolean isFluidAtPosition(BlockPos p_151584_, Predicate<FluidState> p_151585_) {
+        return false;
+    }
+
+    @Override
+    public boolean setBlock(BlockPos p_46947_, BlockState p_46948_, int p_46949_, int p_46950_) {
+        return false;
+    }
+
+    @Override
+    public boolean removeBlock(BlockPos p_46951_, boolean p_46952_) {
+        return false;
+    }
+
+    @Override
+    public boolean destroyBlock(BlockPos p_46957_, boolean p_46958_, @Nullable Entity p_46959_, int p_46960_) {
+        return false;
     }
 }
